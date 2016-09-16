22c8bd30f4806c0cfb94798de0abfebbacb5cc2a
Benjamin Russell <benrr101@outlook.com>
2016-09-16 16:56:46 -0700

    Adding fallback if MPD isn't running

--- config/initializers/mpd.rb
+++ config/initializers/mpd.rb
@@ -3,8 +3,12 @@
 # - Ensures that consume mode is enabled on the MPD instance
 
 Rails.application.config.after_initialize do
-  mpd_model = Mpd.new
-
-  # Ensure consume mode is turned of
-  mpd_model.ensure_consume
+  begin
+    # Ensure consume mode is turned on
+    mpd_model = Mpd.new
+    mpd_model.ensure_consume
+  rescue Exception => e
+    Rails.logger.error("Exception during initialization of MPD #{e.message}")
+    Rails.logger.error('MPD functionality will not work')
+  end
 end
