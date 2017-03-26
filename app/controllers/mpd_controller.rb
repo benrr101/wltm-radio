fbf37ba2690bce83acbc471a7375a5b29e5aa196
Benjamin Russell <beruss@microsoft.com>
2017-03-25 22:58:08 -0700

    Fixing issue with missing references in buffer_file

--- app/controllers/mpd_controller.rb
+++ app/controllers/mpd_controller.rb
@@ -16,22 +16,19 @@ class MpdController < ActionController::Base
     # Step 2) Add the track to MPD's queue
     begin
       Rails.logger.info("Adding to MPD playlist: #{File.basename(buffer_file.track.absolute_path)}")
-      file_uri = "file:///#{buffer_file.absolute_path}"
+      file_uri = "file:///#{buffer_file.track.absolute_path}"
       mpd.queue_add(file_uri)
     rescue Exception => e
       Rails.logger.error("Failed to add '#{File.basename(buffer_file.track.absolute_path)}' to MPD: #{e.message}")
       return
     end
 
-    # Step 3) Add the record to the track table if it isn't already in there
-    track_record = Track.create_from_file(buffer_file.track.absolute_path)
-
-    # Step 4) Add the history record for the track
+    # Step 3) Add the history record for the track
     HistoryRecord.create(
       on_behalf_of: buffer_file.on_behalf_of,
       bot_queued: buffer_file.bot_queued,
       played_time: DateTime.now + remaining_time.seconds,
-      track_id: track_record.id
+      track_id: buffer_file.track.id
     )
   end
 
