894f993a4d7faf4cfbfcfe7b2a320945e3edc69a
Benjamin Russell <beruss@microsoft.com>
2017-03-26 23:28:24 -0700

    Not sure why this didn't work before, but fixing art references

---
+++ db/migrate/20170326231900_fix_art_reference.rb
@@ -0,0 +1,12 @@
+class FixArtReference < ActiveRecord::Migration
+  def change
+    Track.find_each(batch_size: 100) do |track|
+      unless track.art_id.nil?
+        next
+      end
+
+      art = Art.create_from_file(track.absolute_path)
+      track.update_attributes(:art_id => art.id)
+    end
+  end
+end
